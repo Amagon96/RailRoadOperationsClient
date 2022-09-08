efad1cc4952dcab4b35bc970ccf06db456a01c0d
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-09-08 13:30:37 -0500

    select is not selecting the value

--- packages/train/src/components/Train/TrainInput.tsx
+++ packages/train/src/components/Train/TrainInput.tsx
@@ -6,35 +6,41 @@ import {
   InputLabel,
   MenuItem,
   OutlinedInput,
-  Select,
+  TextField,
   TableBody,
   TableRow,
   TableContainer,
   Table,
-  TextField,
+  Select,
   TableCell,
 } from "@mui/material";
 import React, { useState } from "react";
 import TrainLabel from "./TrainLabel";
 
-// const destinations = [
-//   {
-//     value: '1',
-//     label: 'Houston',
-//   },
-//   {
-//     value: '2',
-//     label: 'Chicago',
-//   },
-//   {
-//     value: '3',
-//     label: 'LA',
-//   },
-// ];
+const destinations = [
+  {
+    value: "1",
+    label: "Houston",
+  },
+  {
+    value: "2",
+    label: "Chicago",
+  },
+  {
+    value: "3",
+    label: "LA",
+  },
+];
 
 const TrainInput = () => {
-  const [destination, setDestination] = useState("");
+  const [destination, setDestination] = useState([]);
   const [receiver, setReceiver] = useState("");
+
+  const handleChangeDestination = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDestination([]);
+  };
 
   return (
     <TableRow>
@@ -43,18 +49,26 @@ const TrainInput = () => {
       </TableCell>
       <TableCell>
         <FormControl fullWidth>
-          <InputLabel id="dest-id">Destination</InputLabel>
-          <Select
-            labelId="dest-id"
+          {/* <InputLabel id="dest-id">Destination</InputLabel> */}
+          <TextField
+            id="standard-select-currency"
+            select
             label="Destination"
             value={destination}
-            input={<OutlinedInput />}
+            onChange={handleChangeDestination}
+            helperText="Destination"
+            variant="standard"
           >
-            {/* Meter un map */}
+            {destinations.map((option) => (
+            <MenuItem key={option.label} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+            {/* Meter un map
             <MenuItem value="Houston">Houston</MenuItem>
             <MenuItem value="Chicago">Chicago</MenuItem>
-            <MenuItem value="LA">LA</MenuItem>
-          </Select>
+            <MenuItem value="LA">LA</MenuItem> */}
+          </TextField>
         </FormControl>
       </TableCell>
       <TableCell>
@@ -73,7 +87,9 @@ const TrainInput = () => {
           </Select>
         </FormControl>
       </TableCell>
-      <TableCell><Button>Add</Button></TableCell>
+      <TableCell>
+        <Button>Add</Button>
+      </TableCell>
     </TableRow>
   );
 };
