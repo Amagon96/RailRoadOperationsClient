70ce54bfbbba3db460a498307f82cc18c2f1d64f
Luis Lizarraga <llizarraga@nearsoft.com>
2022-09-14 10:46:07 -0500

    Styking components

--- packages/classification/src/components/ClassificationComponent.tsx
+++ packages/classification/src/components/ClassificationComponent.tsx
@@ -1,18 +1,32 @@
 import {Classification} from "../../types/Classification";
 import React, {useCallback, useEffect, useState} from "react";
 import Axios from "axios";
+import ClassificationItem from "./ClassificationItem";
+import { TableRow, TableContainer, Grid, Table } from "@mui/material";
+
 
 const ClassificationComponent = (props: { type: string, classifications: Array<Classification>}) => {
 
   return (
-    <div>
-      <h1>{props.type}</h1>
-      {props.classifications.map((classification) => (
-        <div key={classification.id}>
-          <h2>{classification.name} - {classification.classification} - {classification.id}</h2>
-        </div>
-      ))}
-    </div>
+    <Grid container>
+        <Grid xs={12}>
+            <h1>{props.type}</h1>
+            <TableContainer>
+                <Table>
+                {props.classifications.map(({id, classification, name}) => (
+                    <TableRow key={id}>
+                        <ClassificationItem
+                            id={id}
+                            name={name}
+                            classification={classification}
+                            onRemove={()=>{}}
+                        />
+                    </TableRow>
+                ))}
+                </Table>
+            </TableContainer>
+        </Grid>
+    </Grid>
   )
 }
 
