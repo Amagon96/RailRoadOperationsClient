1c26e2b1fd4f5761b2539efa5d87c025207e50b8
eduardoesparza <eduebernal@gmail.com>
2022-09-15 13:53:43 -0700

    fix imports, again

--- packages/train/src/App.tsx
+++ packages/train/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import InputCard from "./components/input/InputCard";
-import OutputCard from "./components/output/OutputCard";
+import InputCard from "./components/input/input-card";
+import OutputCard from "./components/output/output-card";
 import { ITrainCar } from "./types/TrainCar";
 import { Box, Typography } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
--- packages/train/src/IL-train.tsx
+++ packages/train/src/IL-train.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
-import App from "./App";
+import App from "./app";
 
 const lifecycles = singleSpaReact({
   React,
